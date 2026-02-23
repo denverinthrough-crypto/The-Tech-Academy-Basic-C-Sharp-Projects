70348f84a856506ce9dc48eca23335b27da86795
denverinthrough-crypto <denverinthrough@gmail.com>
2026-02-22 20:02:32 -0500

    String functions

---
+++ Basic_C#_Programs/Strings_Functions/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringFucntions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string name = "Dinber";
+            //    string quote = "The man said, \"Hello\", Dinber. \n Hello on a new line . \n \t Hello on a tab.";
+
+            //    bool trueOrFalse = name.Contains("s");
+            //    trueOrFalse = name.EndsWith("s");
+
+            //    int  length = name.Length;
+
+            //    name = name.ToUpper();
+
+            //    name = name.ToLower();
+
+            //    Console.WriteLine(name);
+            //    Console.ReadLine();
+
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append("My name is Dinber");
+
+
+
+            Console.WriteLine(sb);
+            Console.ReadLine();
+        }
+    }
+}
