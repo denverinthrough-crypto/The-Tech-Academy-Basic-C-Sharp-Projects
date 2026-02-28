983ca4824f468224664feae74c96f815ea84e254
denverinthrough-crypto <denverinthrough@gmail.com>
2026-02-27 20:35:26 -0500

    Added assingment part 1 and 2 with infinite loop and array of strings

--- Basic_C#_Programs/Iterations/Program.cs
+++ Basic_C#_Programs/Iterations/Program.cs
@@ -50,20 +50,69 @@ class Program
         //    Console.ReadLine();
         //}
 
-        List<int> testScores = new List<int>() { 98, 99, 12, 74, 23, 99 };
+        //List<int> testScores = new List<int>() { 98, 99, 12, 74, 23, 99 };
 
-        List<int> passingScores = new List<int>();
+        //List<int> passingScores = new List<int>();
 
-        foreach(int score in testScores)
+        //foreach(int score in testScores)
+        //{
+        //    if (score > 85)
+        //    {
+        //        passingScores.Add(score);
+        //    }
+        //}
+
+        //Console.WriteLine(passingScores.Count);
+        //Console.ReadLine();
+
+
+        // ************** ASSIGNEMENT PART 1 *************
+
+        // array of strings
+        string[] fruits = { "Apple ", "Banana ", "Cherry ", "Grapes ", "Mango " };
+
+        // User's input text
+        Console.WriteLine("Enter some text: ");
+        string userText = Console.ReadLine(); // reads full line including spaces
+
+        // loop through each string and append user's text 
+        for (int i = 0; i < fruits.Length; i++)
         {
-            if (score > 85)
+            fruits[i] += userText; // add userText to the end of the string
+        }
+
+        // Second loop to print each updated string
+        Console.WriteLine("\nUpdated array: ");
+        for (int i = 0; i < fruits.Length; i++)
+        {
+            Console.WriteLine(fruits[i]);
+            Console.ReadLine();
+        }
+
+
+        // *************** ASSIGNMENT PART 2 *****************
+
+        // *********** INFINITE LOOP *************
+
+        int counter = 0; // initialize counter to 0
+        while (true) // loop starts as infinite
+        {
+            Console.WriteLine("Loop iteration: " + (counter + 1));
+            counter++; // increment counter each interation
+            if (counter >= 5) // check if counter reached 5
+
             {
-                passingScores.Add(score);
+                break; // break exits the loop completely
             }
         }
 
-        Console.WriteLine(passingScores.Count);
-        Console.ReadLine();
+        Console.WriteLine("Loop finsihed!");
+
+
+        
+  
+        
+
 
 
 
