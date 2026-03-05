937a30c5b3767cd43b56223c9dd8f437439a0feb
denverinthrough-crypto <denverinthrough@gmail.com>
2026-03-04 19:10:37 -0500

    add math console app with separate class file

---
+++ Basic_C#_Programs/Assignments/MathOperations.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MathConsoleApp
+{
+    public class MathOperations
+    {
+        // Multiplies the number by 2 and returns the result
+        public int Double(int number)
+        {
+            return number * 2;
+        }
+
+        // Multiplies the number by itself and returns the result
+        public int Square(int number)
+        {
+            return number * number;
+        }
+
+        // Adds 100 to the number and returns the result
+        public int AddOneHundred(int number)
+        {
+            return number + 100;
+        }
+
+
+    }
+}
---
+++ Basic_C#_Programs/Assignments/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MathConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ask the user to enter a number
+            Console.Write("Enter a number: ");
+            int number = int.Parse(Console.ReadLine());
+
+            // Create an instance of the MathOperations class
+            MathOperations math = new MathOperations();
+
+            // Call each method and display the result
+            Console.WriteLine("Double: " + math.Double(number));
+            Console.WriteLine("Square: " + math.Square(number));
+            Console.WriteLine("Add 100: " + math.AddOneHundred(number));
+        }
+    }
+}
